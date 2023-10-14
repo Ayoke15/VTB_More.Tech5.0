84a28aebcb4a0f4cc11e24e1fb73efb58ead93aa
Egor Elkov <elkov.egor@gmail.com>
2023-10-14 14:13:30 +0300

    header pretty much done

--- client/src/components/UI/Header.jsx
+++ client/src/components/UI/Header.jsx
@@ -3,11 +3,17 @@ import logo from './VTB_logo_ru.png';
 
 const Header = () => {
   return (
+    <>
     <header>
+      <div className='figure_mini'>
+      </div>
+      <div className='figure'>
+      </div>
         <nav>
           <img src={logo} className='logo'></img>
         </nav>
     </header>
+    </>
   );
 };
 
